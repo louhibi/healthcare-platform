ce194ee3791de5ab1d350285130e12dc25c006b6
Mohammed Rida louhibi <louhibi@gmail.com>
2025-08-25 20:55:36 -0400

    chore(deps): add go.sums and update config-service deps

--- services/config-service/database.go
+++ services/config-service/database.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "os"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 // InitDB initializes database connection for config service
 func InitDB() (*sql.DB, error) {
-    host := getEnv("DB_HOST", "localhost")
-    port := getEnv("DB_PORT", "5432")
-    user := getEnv("DB_USER", "postgres")
-    pass := getEnv("DB_PASSWORD", "postgres")
-    name := getEnv("DB_NAME", "config_service_db")
+	host := getEnv("DB_HOST", "localhost")
+	port := getEnv("DB_PORT", "5432")
+	user := getEnv("DB_USER", "postgres")
+	pass := getEnv("DB_PASSWORD", "postgres")
+	name := getEnv("DB_NAME", "config_service_db")
 
-    dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, pass, name)
-    db, err := sql.Open("postgres", dsn)
-    if err != nil { return nil, err }
-    if err := db.Ping(); err != nil { return nil, err }
-    return db, nil
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, pass, name)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
 
 // RunMigrations executes versioned migrations
